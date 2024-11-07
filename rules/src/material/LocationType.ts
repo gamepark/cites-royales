52318e2f705bca78188ee6b08209540c95c482a4
Romain Fromi <romain.fromi@gmail.com>
2024-11-07 10:39:27 +0100

    List Location types required for the setup

--- rules/src/material/LocationType.ts
+++ rules/src/material/LocationType.ts
@@ -1,2 +1,18 @@
 export enum LocationType {
+  VictoryPointsTrackSpot,
+  SeasonsCardsStack,
+  DrawPile, // of Subject cards
+  Discard, // of Subject cards
+  Reserve, // of Subject cards
+  Market, // of Subject cards
+  MarketLineBeginning, // for Market half-sized cards
+  VictoryPointsSpace, // on the track
+  MarketTokenSpot, // in front of a player
+  OnSeasonCards, // used Market tokens
+  CityBoardSpot,
+  HelpCardSpot,
+  HeroSpot,
+  PlayerHand,
+  PlayerArea, // For cards face-down in front of a player waiting to be revealed
+  InCity // Built cards
 }
