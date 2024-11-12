61065404d3621b022a3dc64f751ad615c9fde3ce
Romain Fromi <romain.fromi@gmail.com>
2024-11-12 11:27:05 +0100

    Use ListLocator for SeasonsCardsStack

--- app/src/locators/SeasonsCardsStackLocator.ts
+++ app/src/locators/SeasonsCardsStackLocator.ts
@@ -1,11 +1,9 @@
-import { Locator } from '@gamepark/react-game'
-import { Location } from '@gamepark/rules-api'
+import { ListLocator } from '@gamepark/react-game'
 import { seasonCardDescription } from '../material/SeasonCardDescription'
 
-class SeasonsCardsStackLocator extends Locator {
-  getCoordinates(location: Location) {
-    return { x: 20 + location.x! * seasonCardDescription.width }
-  }
+class SeasonsCardsStackLocator extends ListLocator {
+  coordinates = { x: 20 }
+  gap = { x: seasonCardDescription.width }
 }
 
 export const seasonsCardsStackLocator = new SeasonsCardsStackLocator()
