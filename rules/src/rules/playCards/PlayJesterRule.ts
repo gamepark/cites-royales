44da488e3b23339085f0cd47b8414601bbf6f406
Romain Fromi <romain.fromi@gmail.com>
2024-11-29 12:04:17 +0100

    Simplify Jester rule

--- rules/src/rules/playCards/PlayJesterRule.ts
+++ rules/src/rules/playCards/PlayJesterRule.ts
@@ -1,4 +1,3 @@
-import { isMoveItemType, ItemMove } from '@gamepark/rules-api'
 import { LocationType } from '../../material/LocationType'
 import { MaterialType } from '../../material/MaterialType'
 import { RuleId } from '../RuleId'
@@ -6,13 +5,12 @@ import { CardEffectRule } from './CardEffectRule'
 
 export class PlayJesterRule extends CardEffectRule {
   onRuleStart() {
-    return this.material(MaterialType.SubjectCard)
-      .location(LocationType.DrawPile)
-      .deck()
-      .deal({ type: LocationType.PlayerHand, player: this.player }, 1)
-  }
-  afterItemMove(move: ItemMove) {
-    if (!isMoveItemType(MaterialType.SubjectCard)(move) || move.location.type !== LocationType.PlayerHand) return []
-    return [this.startRule(RuleId.MarketBuy)]
+    return [
+      this.material(MaterialType.SubjectCard)
+        .location(LocationType.DrawPile)
+        .deck()
+        .dealOne({ type: LocationType.PlayerHand, player: this.player }),
+      this.startRule(RuleId.MarketBuy)
+    ]
   }
 }
