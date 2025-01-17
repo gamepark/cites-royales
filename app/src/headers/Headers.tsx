1cb85f044bc239ff90f43650f1fc9891a2476b00
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-01-17 10:49:46 +0100

    SetupBuildHeader

--- app/src/headers/Headers.tsx
+++ app/src/headers/Headers.tsx
@@ -5,9 +5,11 @@ import { MarketBuyHeader } from './MarketBuyHeader'
 import { PlayCardHeader } from './PlayCardHeader'
 import { SetupDraftHeader } from './SetupDraftHeader'
 import { CitiesConstructionHeader } from './CitiesConstructionHeader'
+import { SetupBuildHeader } from './SetupBuildHeader'
 
 export const Headers: Partial<Record<RuleId, ComponentType>> = {
   [RuleId.SetupDraft]: SetupDraftHeader,
+  [RuleId.SetupBuild]:SetupBuildHeader,
   [RuleId.PlayCard]: PlayCardHeader,
   [RuleId.MarketBuy]: MarketBuyHeader,
   [RuleId.CitiesConstruction]:CitiesConstructionHeader
---
+++ app/src/headers/SetupBuildHeader.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from 'react-i18next'
+import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { NobleColor } from '@gamepark/cites-royales/NobleColor'
+import { CitesRoyalesRules } from '@gamepark/cites-royales/CitesRoyalesRules'
+
+export const SetupBuildHeader = () => {
+  const { t } = useTranslation()
+  const me = usePlayerId<NobleColor>()
+  const rules = useRules<CitesRoyalesRules>()!
+  if(me !== undefined && rules.isTurnToPlay(me)) {
+    return <>{t('header.setup-build.you')}</>
+  } else {
+    const activePlayers = rules.game.rule!.players!
+    if (activePlayers.length > 1) {
+      return <>{t('header.setup-build.players')}</>
+    } else {
+      return <SetupBuildHeaderPlayer player={activePlayers[0]}/>
+    }
+  }
+}
+
+const SetupBuildHeaderPlayer = ({ player }: { player: NobleColor }) => {
+  const { t } = useTranslation()
+  const playerName = usePlayerName(player)
+  return <>{t('header.setup-build.player', { player: playerName })}</>
+}
