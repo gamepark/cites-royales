e244a8da7e744a44d098c83c59d8e83170391b63
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-01-22 11:58:01 +0100

    CitiesConstructionHeader

--- app/src/headers/CitiesConstructionHeader.tsx
+++ app/src/headers/CitiesConstructionHeader.tsx
@@ -1,16 +1,36 @@
-import { Trans } from 'react-i18next'
-import { PlayMoveButton, useLegalMove } from '@gamepark/react-game'
+import { Trans, useTranslation } from 'react-i18next'
+import { PlayMoveButton, useLegalMove, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
 import { isCustomMoveType } from '@gamepark/rules-api'
 import { CustomMoveType } from '@gamepark/cites-royales/rules/CustomMoveType'
+import { NobleColor } from '@gamepark/cites-royales/NobleColor'
+import { CitesRoyalesRules } from '@gamepark/cites-royales/CitesRoyalesRules'
 
 export const CitiesConstructionHeader = () => {
+  const { t } = useTranslation()
+  const me = usePlayerId<NobleColor>()
+  const rules = useRules<CitesRoyalesRules>()!
   const pass = useLegalMove(isCustomMoveType(CustomMoveType.Pass))
-  return (
-    <Trans
-      defaults="header.build.you"
-      components={{
-        pass: <PlayMoveButton move={pass}/>
-      }}
-    />
-  )
+  if(me !== undefined && rules.isTurnToPlay(me)){
+    return (
+      <Trans
+        defaults="header.build.you"
+        components={{
+          pass: <PlayMoveButton move={pass}/>
+        }}
+      />
+    )
+  } else {
+    const activePlayers = rules.game.rule!.players!
+    if(activePlayers.length > 1){
+      return <>{t('header.setup-draft.players')}</>
+    } else {
+      return <CitiesConstructionPlayer player={activePlayers[0]}/>
+    }
+  }
+}
+
+const CitiesConstructionPlayer = ({ player }: { player: NobleColor }) => {
+  const { t } = useTranslation()
+  const playerName = usePlayerName(player)
+  return <>{t('header.build.player', { player: playerName })}</>
 }
