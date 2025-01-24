d660f12f65bcc861f57e4c657ce3906c4d7ee6a2
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-01-24 13:17:37 +0100

    Modify player hand angle

--- app/src/locators/PlayerHandLocator.ts
+++ app/src/locators/PlayerHandLocator.ts
@@ -5,17 +5,15 @@ import { cityBoardSpotLocator } from './CityBoardSpotLocator'
 class PlayerHandLocator extends HandLocator {
   coordinates = { x: 39, y: 25 }
 
-  radius = 100 // default value
-  baseAngle = 0 // default value
-  maxAngle = 15 // default value
-  gapMaxAngle = 3 // default value
-  clockwise = true // default value
-
-  // TODO : Resserrer les cartes ou les déplacer pour prendre moins de place / voir le Héro
   getCoordinates(location: Location, context: MaterialContext) {
     const playerIndex = getRelativePlayerIndex(context, location.player)
     const { x = 0, y = 0 } = cityBoardSpotLocator.getCoordinates(location, context)
-    return playerIndex === 0 ? { x: x - 40, y: y - 5 } : { x: x + 35, y: y - 8 }
+    return playerIndex === 0 ? { x: x - 40, y: y - 5 } : { x: x + 38, y: y - 8 }
+  }
+
+  getGapMaxAngle(location: Location, context: MaterialContext) {
+    const playerIndex = getRelativePlayerIndex(context, location.player)
+    return playerIndex === 0 ? 3 : 2
   }
 }
 
