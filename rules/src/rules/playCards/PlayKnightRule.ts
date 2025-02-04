0b060120cfb3d681aaa0c36b5213b6679e5cba32
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-02-04 15:13:27 +0100

    Fix white cards with knight

--- rules/src/rules/playCards/PlayKnightRule.ts
+++ rules/src/rules/playCards/PlayKnightRule.ts
@@ -1,5 +1,5 @@
 import { isMoveItemType, ItemMove, Material, MaterialMove } from '@gamepark/rules-api'
-import { cities } from '../../material/City'
+import { cities, City } from '../../material/City'
 import { LocationType } from '../../material/LocationType'
 import { MaterialType } from '../../material/MaterialType'
 import { getSubjectCity, getSubjectType } from '../../material/Subject'
@@ -18,12 +18,23 @@ export class PlayKnightRule extends CardEffectRule {
 
         const highestCard = cardsInCity.filter((card) => card.location.id === city).maxBy((card) => card.location.x!)
 
-        const cardCanBePlacedInCity = this.cardCanBePlacedInCity(highestCard)
+        if(getSubjectCity(highestCard.getItem()?.id) === undefined) {
+          for(const cityColor of cities){
+            const cardCanBePlacedInCity = this.cardCanBePlacedInCity(highestCard, cityColor)
 
-        if (cardCanBePlacedInCity) {
-          moves.push(
-            ...highestCard.moveItems((item) => ({ type: LocationType.InCity, player, id: getSubjectCity(item.id) }))
-          )
+            if (cardCanBePlacedInCity) {
+              moves.push(
+                ...highestCard.moveItems({ type: LocationType.InCity, player, id: cityColor })
+              )
+            }
+          }
+        } else {
+          const cardCanBePlacedInCity = this.cardCanBePlacedInCity(highestCard, city)
+          if (cardCanBePlacedInCity) {
+            moves.push(
+              ...highestCard.moveItems({ type: LocationType.InCity, player, id: city })
+            )
+          }
         }
       }
     }
@@ -33,19 +44,17 @@ export class PlayKnightRule extends CardEffectRule {
     if (!isMoveItemType(MaterialType.SubjectCard)(move) || move.location.type !== LocationType.InCity) return []
     return [this.nextRule]
   }
-  cardCanBePlacedInCity(card:Material){
+  cardCanBePlacedInCity(card:Material, city: City){
     if(card.length < 1) return false
     const cardItem = card.getItem()
-    const cardColor = cardItem!.location.id
     const cardType = getSubjectType(cardItem!.id)
 
-    const cardsInSameCity = this.material(MaterialType.SubjectCard).location(LocationType.InCity).player(this.player).filter((cityCard) => cityCard.location.id === cardColor)
+    const cardsInSameCity = this.material(MaterialType.SubjectCard).location(LocationType.InCity).player(this.player).filter((cityCard) => getSubjectCity(cityCard.id) === city)
 
     const higherCityCards = cardsInSameCity.filter((cityCard) => {
-      return getSubjectType(cityCard.id) > cardType
+      return getSubjectType(cityCard.id) >= cardType
     })
 
-
-    return higherCityCards.length < 1 || cardsInSameCity.length === 0
+    return  (higherCityCards.length < 1 || cardsInSameCity.length === 0)
   }
 }
