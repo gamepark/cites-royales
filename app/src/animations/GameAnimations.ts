56aba1200546b795fb45c2a38c79a4b1ec02c5da
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-02-05 14:55:56 +0100

    Animation de déplacement de pions uniquement lors des cityScorings

--- app/src/animations/GameAnimations.ts
+++ app/src/animations/GameAnimations.ts
@@ -1,9 +1,36 @@
 import { MaterialGameAnimations } from '@gamepark/react-game'
 import { isMoveItemType } from '@gamepark/rules-api'
 import { MaterialType } from '@gamepark/cites-royales/material/MaterialType'
+import { RuleId } from '@gamepark/cites-royales/rules/RuleId'
 
 export const gameAnimations = new MaterialGameAnimations()
 
 gameAnimations.when()
+  .rule(RuleId.PurpleMajority)
   .move((move) => isMoveItemType(MaterialType.NobleToken)(move))
-  .duration(3)
+  .duration(5)
+
+gameAnimations.when()
+  .rule(RuleId.YellowMajority)
+  .move((move) => isMoveItemType(MaterialType.NobleToken)(move))
+  .duration(5)
+
+gameAnimations.when()
+  .rule(RuleId.PinkMajority)
+  .move((move) => isMoveItemType(MaterialType.NobleToken)(move))
+  .duration(5)
+
+gameAnimations.when()
+  .rule(RuleId.RedMajority)
+  .move((move) => isMoveItemType(MaterialType.NobleToken)(move))
+  .duration(5)
+
+gameAnimations.when()
+  .rule(RuleId.BlueMajority)
+  .move((move) => isMoveItemType(MaterialType.NobleToken)(move))
+  .duration(5)
+
+gameAnimations.when()
+  .rule(RuleId.GreenMajority)
+  .move((move) => isMoveItemType(MaterialType.NobleToken)(move))
+  .duration(5)
