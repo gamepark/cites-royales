1b4a467e55cf99de28e556ec838be144e0434f5b
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-03-11 17:41:20 +0100

    board helper image

--- app/src/material/help/CityBoardHelp.tsx
+++ app/src/material/help/CityBoardHelp.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react'
-import { MaterialHelpProps, usePlayerName } from '@gamepark/react-game'
+import { MaterialComponent, MaterialHelpProps, usePlayerName } from '@gamepark/react-game'
 import { Trans } from 'react-i18next'
 import { cities } from '@gamepark/cites-royales/material/City'
 import { css } from '@emotion/react'
+import { MaterialType } from '@gamepark/cites-royales/material/MaterialType'
+import { NobleColor } from '@gamepark/cites-royales/NobleColor'
 
 export const CityBoardHelp: FC<MaterialHelpProps> = (props) => {
   const { item } = props
@@ -11,6 +13,8 @@ export const CityBoardHelp: FC<MaterialHelpProps> = (props) => {
   return (
     <>
       <h2 css={titleCss}><Trans defaults={'description.city-board.title'} values={{player:playerName}}/></h2>
+
+      <MaterialComponent type={MaterialType.CityBoard} itemId={NobleColor.Beige} css={boardCss}/>
       {cities.map(city => {
         return city !== undefined && <p key={city}><Trans defaults={'description.city-board.'+city}><strong/></Trans></p>
       })}
@@ -20,4 +24,8 @@ export const CityBoardHelp: FC<MaterialHelpProps> = (props) => {
 
 const titleCss = css`
   margin-bottom: 0.5em !important;
+`
+const boardCss = css`
+  font-size: 0.7em;
+  position: static;
 `
